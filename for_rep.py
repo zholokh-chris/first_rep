4ad0cf12ba711ce6b30dc2abe6b97b6368d30a6f
zholokh-chris <120093173+zholokh-chris@users.noreply.github.com>
2022-12-10 22:02:37 +0300

    Update for_rep.py

--- for_rep.py
+++ for_rep.py
@@ -1,6 +1,6 @@
 n = int(input())
 if n % 10 == 1 and n != 11:
-    print('Мне' ,n ,'год')
+    print('Мне', n, 'год')
 elif n % 10 == 2 and n != 12 or n % 10 == 3 and n != 13 or n % 10 == 4 and n != 14:
     print('Мне', n, 'года')
 else:
