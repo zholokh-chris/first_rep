45f6a149c04e4e3eabfaa1e29a51aea2ad8132f0
zholokh-chris <zholokh.chris@gmail.com>
2022-12-10 21:42:51 +0300

    task with years

--- for_rep.py
+++ for_rep.py
@@ -1,1 +1,7 @@
-"hiiii~~~"
+n = int(input())
+if n % 10 == 1 and n != 11:
+    print('Мне' ,n ,'год')
+elif n % 10 == 2 and n != 12 or n % 10 == 3 and n != 13 or n % 10 == 4 and n != 14:
+    print('Мне', n, 'года')
+else:
+    print('Мне', n, 'лет')
